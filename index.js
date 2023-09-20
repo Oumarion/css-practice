8e840870bf615e73ffb10cb38dea6cdfd201c697
OumarOuattara <greatoumar@hotmail.de>
2023-09-20 10:38:31 +0200

    Added CSS Practice

---
+++ index.js
@@ -0,0 +1,6 @@
+const inner = document.querySelector(".inner");
+inner.style.backgroundColor = "lightGreen";
+inner.textContent = "Hello";
+inner.style.textAlign = "center"
+inner.style.paddingTop = "10px";
+inner.style.fontSize = "35px";
